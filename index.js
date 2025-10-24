f5317b5f2b4f2d381ddfcd8a32352fe29274fa09
saritagore21@gmail.com <rahulgore34@gmail.com>
2025-10-24 13:34:01 +0530

    cors enables

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT|| 3000;
-
+app.use(cors());
 app.get('/', (req, res) => {
   res.send('Hello World test123');
 });
